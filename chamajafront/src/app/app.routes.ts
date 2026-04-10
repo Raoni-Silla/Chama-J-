657ebc1ace8d1c78509ff9cb6f2f00d7a190e425
Raoni-Silla <raonisilla@gmail.com>
2026-04-10 16:42:50 -0300

    front

--- chamajafront/src/app/app.routes.ts
+++ chamajafront/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { Landingpage } from './screens/landingpage/landingpage';
 import { Login } from './screens/login/login';
+import { Register } from './screens/register/register';
 
 export const routes: Routes = [    
  { path: '', component: Landingpage },
- { path: 'login', component: Login}
+ { path: 'login', component: Login},
+ { path:'register', component: Register}
 ];
--- chamajafront/src/app/screens/register/register.ts
+++ chamajafront/src/app/screens/register/register.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { Defaultbutton } from '../../components/defaultbutton/defaultbutton';
+import { FormBuilder, FormGroup, FormsModule, NgModel, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [Defaultbutton],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+}
